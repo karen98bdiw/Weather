f051fe6255cbde902b87b05a500ed96005f555cc
karen98bdiw <ghazaryan.deb@mail.ru>
2019-06-14 23:21:01 +0400

    +intro -bugFixing and testing

--- app/src/main/java/com/example/weather/IntroActivity.java
+++ app/src/main/java/com/example/weather/IntroActivity.java
@@ -1,90 +1,137 @@
 package com.example.weather;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntroActivity extends AppCompatActivity {
 
-//    static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
-//    static final String APPID = "9328aa88a8e3e7336e7ca0df235a226a";
-//
-//    private List<View> introPages;
-//    private LayoutInflater inflater;
-//    private ViewPager viewPager;
-//    private Retrofit retrofit;
-//    private Gson gson;
-//    private CompositeDisposable compositeDisposable;
-//    private View view;
-//
+    private ImageView weatherKindIcon;
+    private TextView weatherDescription;
+    private Button skipBtn;
+    private Gson gson;
+    private OneDayWeather curentDayWeather;
+    private List<OneDayWeather> allWeekWeather;
+    private LayoutInflater inflater;
+    private ViewPager viewPager;
+    private List<View> pages;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-//
-//        gson = new Gson();
-//
-//        compositeDisposable = new CompositeDisposable();
-//
-//        introPages = new ArrayList<>();
-//
-//        viewPager = findViewById(R.id.introActivityViewPager);
-//
-//        inflater = LayoutInflater.from(this);
-//
-//         view = inflater.inflate(R.layout.app_intro_viewpager_item,null);
-//
-//
-//         introPages.add(view);
-//        viewPager.setCurrentItem(0);
-//
-//        viewPager.setAdapter(new IntroViewPagerAdapter(introPages));
-//
-//        retrofit = new Retrofit.Builder()
-//                  .baseUrl(BASE_URL)
-//                  .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//                  .addConverterFactory(GsonConverterFactory.create(gson))
-//                  .build();
-//
-//        final RetrofitRequestInterface requestInterface = retrofit.create(RetrofitRequestInterface.class);
-//
-//        requestInterface.getApiResponse("524901","9328aa88a8e3e7336e7ca0df235a226a")
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new SingleObserver<Response>() {
-//                    @Override
-//                    public void onSubscribe(Disposable d) {
-//                        compositeDisposable.add(d);
-//                    }
-//
-//                    @Override
-//                    public void onSuccess(final Response response) {
-//
-//                        view.findViewById(R.id.skipBtn).setOnClickListener(new View.OnClickListener() {
-//                            @Override
-//                            public void onClick(View v) {
-//                                Intent intent = new Intent(IntroActivity.this,MainActivity.class);
-//                                intent.putExtra("ResultWeather",response);
-//                                startActivity(intent);
-//                            }
-//                        });
-//
-//                        List<OneDayWeather> weekDaysWeather = takeWeatherInUsefullFormat(response);
-//
-//                        for (OneDayWeather o:weekDaysWeather){
-//                            Log.e("rrr", "onSuccess: " + o.getMaxTemp() );
-//                        }
-//
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e("no","somethingiswrong");
-//                        e.printStackTrace();
-//                    }
-//                });
+
+        viewPager = findViewById(R.id.introActivityViewPager);
+
+        gson = new Gson();
+        allWeekWeather = new ArrayList<>();
+        pages = new ArrayList<>();
+
+        File file = getFilesDir();
+        File[] files = file.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".wt");
+            }
+        });
+
+        if(files.length > 0){
+
+            for(File f:files){
+                StringBuilder sb = new StringBuilder();
+                try {
+                    FileInputStream fis = new FileInputStream(f);
+                    InputStreamReader inp = new InputStreamReader(fis);
+                    BufferedReader bf = new BufferedReader(inp);
+                    String line;
+                    while((line = bf.readLine()) != null){
+                        sb.append(line);
+                    }
+                    OneDayWeather oneDayWeather = gson.fromJson(sb.toString(),OneDayWeather.class);
+                    allWeekWeather.add(oneDayWeather);
+                    Log.e("hey",allWeekWeather.size() + "");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            curentDayWeather = allWeekWeather.get(0);
+            drowIntro();
+
+        }else{
+            Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+
+
 
     }
 
+    public void drowIntro(){
+        inflater =  LayoutInflater.from(IntroActivity.this);
+        View dayWeatherView = inflater.inflate(R.layout.app_intro_viewpager_item,null);
+        weatherKindIcon = dayWeatherView.findViewById(R.id.introImage);
+        StringBuilder sb = new StringBuilder();
+        sb.append("http://openweathermap.org/img/w/")
+                .append(curentDayWeather.getWeatherIcon())
+                .append(".png");
+        Log.e("iconU",sb.toString());
+        Glide
+                .with(IntroActivity.this)
+                .load(sb.toString())
+                .into(weatherKindIcon);
+        weatherDescription = dayWeatherView.findViewById(R.id.introText);
+        weatherDescription.setText("At day will be " + curentDayWeather.getWeatherKind());
+        pages.add(dayWeatherView);
 
+        inflater =  LayoutInflater.from(IntroActivity.this);
+        View nightWeatherView = inflater.inflate(R.layout.app_intro_viewpager_item,null);
+        weatherKindIcon = nightWeatherView.findViewById(R.id.introImage);
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append("http://openweathermap.org/img/w/")
+                .append(curentDayWeather.getNightWeatherKind())
+                .append(".png");
+        Log.e("iconU",sb2.toString());
+        Glide
+                .with(IntroActivity.this)
+                .load(sb.toString())
+                .into(weatherKindIcon);
+        weatherDescription = nightWeatherView.findViewById(R.id.introText);
+        weatherDescription.setText("At the night will be " +curentDayWeather.getNightWeatherKind());
+        pages.add(nightWeatherView);
+
+        for(View v:pages){
+            v.findViewById(R.id.skipBtn).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+        viewPager.setAdapter(new IntroViewPagerAdapter(pages));
+        viewPager.setCurrentItem(0);
+    }
 }
